a0fdba093acf1763f8eff38896e2acdb645d6217
jam231 <mateusz.jany@gmail.com>
2014-05-29 20:30:35 +0200

    Check if argument is a valid integer if not then return 404. Add helper for that.

--- lib/webservice.rb
+++ lib/webservice.rb
@@ -27,18 +27,35 @@ class SiaWebservice < Sinatra::Base
     content_type :json
   end
 
+  helpers do
+    # TODO: refactor
+    def redirect_to_404_if_not_an_integer(value)
+      begin
+        Integer(value)
+      rescue ArgumentError
+        halt 404
+      end
+    end
+  end
+
   # {stock_id: ..., best_buy_metric: {...}}
   get '/best_order_metric/best_buy/:stock_id' do |stock_id|
+    stock_id = redirect_to_404_if_not_an_integer stock_id
+   
     { stock_id: stock_id, best_buy_metric: settings.db.get_best_buy_metric(stock_id) }
   end
 
   # {stock_id: ..., best_sell_metric: {...}}
   get '/best_order_metric/best_sell/:stock_id' do |stock_id|
+    stock_id = redirect_to_404_if_not_an_integer stock_id
+   
     { stock_id: stock_id, best_sell_metric: settings.db.get_best_sell_metric(stock_id) }
   end
 
   # {stock_id: ..., best_order_metric: {...}}
   get '/best_order_metric/:stock_id' do |stock_id|
+    stock_id = redirect_to_404_if_not_an_integer stock_id
+    
     { stock_id: stock_id, best_order_metric: settings.db.get_best_order_metric(stock_id) }
   end
 end
